8420d49db6f540e72a34637d2305141a7ff084b0
minion-star <williamalderette799@gmail.com>
2025-02-21 09:38:01 -0600

    add new button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,12 +14,18 @@ const Home = () => {
                 <p className="text-lg md:text-2xl font-bold text-black">
                     Contractors & Construction Managers Since 1989
                 </p>
-                <button className="py-2 px-6 md:py-4 md:px-8 mt-6 rounded-full text-black bg-white hover:bg-black hover:text-white transition duration-500 hover:cursor-pointer" onClick={()=>Navigate("/projects")}>
+                <button className="py-2 px-6 md:py-4 md:px-8 mt-6 rounded-full text-white border-2 border-white hover:bg-white hover:text-black transition duration-500 hover:cursor-pointer" onClick={()=>Navigate("/projects")}>
                     See Our Recent Projects
                 </button>
             </div>
-            <div id="new-idea" className="bg-amber-500 bg-cover flex flex-1 justify-between">
-
+            <div id="new-idea" className="bg-amber-500 bg-cover flex px-6 py-12">
+                <div className="flex-auto">
+                    <p className="text-[48px] text-white">Already have an excellent idea!</p>
+                    <p className="text-[20px] text-white">And want to know whether it is possible to implement it?</p>
+                </div>
+                <div className="flex-auto text-center py-8">
+                    <button className="px-6 py-2 rounded-full text-center text-white border-2 border-white hover:bg-white hover:text-black transition duration-700 cursor-pointer">See New Projects</button>
+                </div>
             </div>
         </div>
     );
