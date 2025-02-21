95f4294595aa3a9462ab62fedfd723bdca2ea6d6
minion-star <williamalderette799@gmail.com>
2025-02-20 20:23:05 -0600

    add hero image

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from "../../public/logo-white.png"
 const Topbar = () => {
     return (
-        <></>
+        <div className="flex flex-row justify-between bg-pink-100/40 fixed w-full mt-0 pl-32 pr-32 top-0 right-0 left-0 z-10">
+            <div id="logo" className="p-4">
+                <img src={logo} alt="logo" className="w-16 h-8"/>
+            </div>
+            <div className="flex flex-row justify-end w-full">
+                <ul className="flex flex-row p-4 align-middle">
+                    <li className="text-white m-1"><Link to="/">Home</Link></li>
+                    <li className="text-white m-1"><Link to="/About">About Us</Link></li>
+                    <li className="text-white m-1"><Link to="/About">Projects</Link></li>
+                    <li className="text-white m-1"><Link to="/About">Blog</Link></li>
+                    <li className="text-white m-1"><Link to="/About">Pages</Link></li>
+                    <li className="text-white m-1"><Link to="/About">Contact Us</Link></li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-        <></>
+        <div>
+            <div id="hero" className="bg-[url(/assets/hero1.jpg)] bg-no-repeat bg-cover p-64 brightness-80">
+                <p className="text-[64px] font-bold font-serif align-middle">COMPLEX PROJECTS</p>
+            </div>
+        </div>
     );
 
 }
