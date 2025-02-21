8996ea7163abfb7da1eb2cd045f000016a9ab490
minion-star <williamalderette799@gmail.com>
2025-02-21 02:06:10 -0600

    resposive

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
-import logo from "../../public/logo-white.png"
+import logo from "../../public/logo-white.png";
+
 const Topbar = () => {
     return (
-        <div className="flex flex-row justify-between bg-pink-100/40 fixed w-full mt-0 pl-32 pr-32 top-0 right-0 left-0 z-10">
-            <div id="logo" className="p-4">
-                <img src={logo} alt="logo" className="w-16 h-8"/>
-            </div>
-            <div className="flex flex-row justify-end w-full">
-                <ul className="flex flex-row p-4 align-middle">
-                    <li className="text-black m-1"><Link to="/">Home</Link></li>
-                    <li className="text-black m-1"><Link to="/About">About Us</Link></li>
-                    <li className="text-black m-1"><Link to="/About">Projects</Link></li>
-                    <li className="text-black m-1"><Link to="/About">Blog</Link></li>
-                    <li className="text-black m-1"><Link to="/About">Pages</Link></li>
-                    <li className="text-black m-1"><Link to="/About">Contact Us</Link></li>
+        <div className="fixed w-full top-0 left-0 bg-pink-100/40 z-10">
+            <div className="container mx-auto flex flex-wrap justify-between items-center px-6 py-4">
+                {/* Logo */}
+                <div id="logo">
+                    <img src={logo} alt="logo" className="w-16 h-8" />
+                </div>
+
+                {/* Navigation */}
+                <ul className="hidden md:flex flex-row space-x-6">
+                    <li><Link to="/" className="text-black hover:text-gray-700 duration-700">Home</Link></li>
+                    <li><Link to="/about" className="text-black hover:text-gray-700 duration-700">About Us</Link></li>
+                    <li><Link to="/projects" className="text-black hover:text-gray-700 duration-700">Projects</Link></li>
+                    <li><Link to="/blog" className="text-black hover:text-gray-700 duration-700">Blog</Link></li>
+                    <li><Link to="/pages" className="text-black hover:text-gray-700 duration-700">Pages</Link></li>
+                    <li><Link to="/contact" className="text-black hover:text-gray-700 duration-700">Contact Us</Link></li>
                 </ul>
+
+                {/* Mobile Menu Button (Optional) */}
+                <button className="md:hidden text-black">☰</button>
             </div>
         </div>
     );
-}
+};
 
 export default Topbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            <div id="hero" className="bg-[url(/assets/hero1.jpg)] bg-no-repeat bg-cover text-center p-64 w-full min-h-screen items-center">
-                <p className="text-[64px] font-bold font-serif text-center text-black">WAY IN BUILDING</p>
-                <p className="text-[24px] font-bold text-center text-black">contractors & construction managers since 1989</p>
-                <button className="py-4 px-8 m-4 rounded-full text-center bg-white text-black hover:bg-black hover:text-white transition duration-1000">See Our Recent Projects</button>
+            <div
+                id="hero"
+                className="bg-[url('/assets/hero1.jpg')] bg-no-repeat bg-cover bg-center text-center flex flex-col items-center justify-center min-h-screen px-4 md:px-0 py-10 md:py-64"
+            >
+                <p className="text-3xl md:text-[64px] font-bold font-serif text-black">
+                    WAY IN BUILDING
+                </p>
+                <p className="text-lg md:text-2xl font-bold text-black">
+                    Contractors & Construction Managers Since 1989
+                </p>
+                <button className="py-2 px-6 md:py-4 md:px-8 mt-6 rounded-full text-black bg-white hover:bg-black hover:text-white transition duration-500">
+                    See Our Recent Projects
+                </button>
             </div>
         </div>
     );
+};
 
-}
-
-export default Home; 
+export default Home;
