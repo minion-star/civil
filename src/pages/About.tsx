be27bb9d1629f193d6d38cae67237f7edacc2fa1
minion-star <williamalderette799@gmail.com>
2025-02-25 01:24:21 -0600

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -82,101 +82,101 @@ const [currentIndex, setCurrentIndex] = useState(0);
     return (
         <>
             <div className="flex flex-col md:flex-row">
-                    <div className="p-4 m-4 flex-1" data-aos="flip-up">
-                        <img src="/assets/excavator.jpg" alt="excavator" className="w-168"/>
-                    </div>
-                    <div className="text-center p-4 m-4 flex-1" data-aos="flip-up">
-                        <p className="text-[48px] text-black font-serif">Providing All Kinds of Construction Services</p>
-                        <hr className="text-amber-500 mt-4"/>
-                        <p className="text-black text-[24px] my-8"> Our construction company was founded 10 years ago, during the peak of the building boom in the US. Since then, we've delivered high-quality, innovative, and sustainable solutions across various sectors.</p>
-                        <p className="text-gray-500 text-md text-left"> We design and build durable structures, efficient transportation systems, and sustainable developments. From site preparation to project completion, we handle grading, drainage, and utility planning while ensuring compliance with regulations. Our transportation solutions enhance mobility through smart road and transit designs. With expert project management, we keep schedules, budgets, and risks under control, ensuring seamless execution. Sustainability drives our work, integrating eco-friendly materials and energy-efficient solutions to minimize environmental impact. Through innovation and precision, we create infrastructure that supports growth, efficiency, and long-term sustainability.</p>
-                        <button className="py-2 px-6 md:py-4 md:px-8 mt-6 rounded-full text-black border-2 border-black hover:bg-black hover:text-white transition duration-700 hover:cursor-pointer">View Portfolios</button>
-                    </div>
+                <div className="p-4 m-4 flex-1" data-aos="flip-up">
+                    <img src="/assets/excavator.jpg" alt="excavator" className="w-168"/>
                 </div>
-                <div className="bg-amber-500 bg-cover flex flex-col px-6 py-12 items-center" id="status">
-                                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 w-full max-w-8xl">
-                                    {statuses.map((status, index) => (
-                                        <div className="flex flex-col items-center" key={index} data-aos="fade-up">
-                                            <p
-                                                className="text-white text-center flex items-center justify-center text-[20px] w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 bg-no-repeat bg-center bg-cover rounded-full shadow-lg"
-                                                style={{ backgroundImage: `url(${status.img})` }}
-                                            >
-                                                {status.status}
-                                            </p>
-                                            <p className="text-[16px] sm:text-[18px] text-white text-center mt-4">{status.title}</p>
-                                        </div>
-                                    ))}
+                <div className="text-center p-4 m-4 flex-1" data-aos="flip-up">
+                    <p className="text-[48px] text-black font-serif">Providing All Kinds of Construction Services</p>
+                    <hr className="text-amber-500 mt-4"/>
+                    <p className="text-black text-[24px] my-8"> Our construction company was founded 10 years ago, during the peak of the building boom in the US. Since then, we've delivered high-quality, innovative, and sustainable solutions across various sectors.</p>
+                    <p className="text-gray-500 text-md text-left"> We design and build durable structures, efficient transportation systems, and sustainable developments. From site preparation to project completion, we handle grading, drainage, and utility planning while ensuring compliance with regulations. Our transportation solutions enhance mobility through smart road and transit designs. With expert project management, we keep schedules, budgets, and risks under control, ensuring seamless execution. Sustainability drives our work, integrating eco-friendly materials and energy-efficient solutions to minimize environmental impact. Through innovation and precision, we create infrastructure that supports growth, efficiency, and long-term sustainability.</p>
+                    <button className="py-2 px-6 md:py-4 md:px-8 mt-6 rounded-full text-black border-2 border-black hover:bg-black hover:text-white transition duration-700 hover:cursor-pointer">View Portfolios</button>
+                </div>
+            </div>
+            <div className="bg-amber-500 bg-cover flex flex-col px-6 py-12 items-center" id="status">
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 w-full max-w-8xl">
+                    {statuses.map((status, index) => (
+                        <div className="flex flex-col items-center" key={index} data-aos="fade-up">
+                            <p
+                                className="text-white text-center flex items-center justify-center text-[20px] w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 bg-no-repeat bg-center bg-cover rounded-full shadow-lg"
+                                style={{ backgroundImage: `url(${status.img})` }}
+                            >
+                                {status.status}
+                            </p>
+                            <p className="text-[16px] sm:text-[18px] text-white text-center mt-4">{status.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="bg-white bg-cover flex flex-col px-12 py-12" id="creative-work">
+                <p className="text-black text-center text-[48px] font-serif">Our Creative Work</p>
+                <div className="flex flex-col md:flex-row mt-16">
+                    {[
+                        [
+                            ["/assets/pf1.jpg", "Project-01", "description-01"],
+                            ["/assets/pf2.jpg", "Project-02", "description-02"],
+                            ["/assets/pf3.jpg", "Project-03", "description-03"]
+                        ],                
+                        [
+                            ["/assets/pf4.jpg", "Project-04", "description-04"],
+                            ["/assets/pf5.jpg", "Project-05", "description-05"],
+                            ["/assets/pf6.jpg", "Project-06", "description-06"]
+                        ],
+                        [
+                            ["/assets/pf7.jpg", "Project-07", "description-07"],
+                            ["/assets/pf8.jpg", "Project-08", "description-08"],
+                            ["/assets/pf9.jpg", "Project-09", "description-09"]
+                        ]
+                    ].map((column, colIndex) => (
+                        <div key={colIndex} className="flex flex-1 flex-col mx-4">
+                            {column.map(([img, title, description], index) => (
+                                <div
+                                    key={index}
+                                    className="relative flex-auto mt-8 bg-no-repeat bg-cover bg-center min-h-[200px] max-h-[400px] w-full rounded-lg shadow-lg flex items-center justify-center group cursor-pointer"
+                                    style={{ backgroundImage: `url(${img})` }}  data-aos="fade-up"
+                                >
+                                    {/* Hover Overlay */}
+                                    <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center rounded-lg shadow-lg justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-50 transition-opacity duration-700">
+                                        <p className="text-2xl">{title}</p>
+                                        <p className="text-sm mt-2">{description}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="bg-white bg-cover flex flex-col px-12 py-12" id="creative-work">
-                                <p className="text-black text-center text-[48px] font-serif">Our Creative Work</p>
-                                <div className="flex flex-col md:flex-row mt-16">
-                                    {[
-                                        [
-                                            ["/assets/pf1.jpg", "Project-01", "description-01"],
-                                            ["/assets/pf2.jpg", "Project-02", "description-02"],
-                                            ["/assets/pf3.jpg", "Project-03", "description-03"]
-                                        ],
-                                        [
-                                            ["/assets/pf4.jpg", "Project-04", "description-04"],
-                                            ["/assets/pf5.jpg", "Project-05", "description-05"],
-                                            ["/assets/pf6.jpg", "Project-06", "description-06"]
-                                        ],
-                                        [
-                                            ["/assets/pf7.jpg", "Project-07", "description-07"],
-                                            ["/assets/pf8.jpg", "Project-08", "description-08"],
-                                            ["/assets/pf9.jpg", "Project-09", "description-09"]
-                                        ]
-                                    ].map((column, colIndex) => (
-                                        <div key={colIndex} className="flex flex-1 flex-col mx-4">
-                                            {column.map(([img, title, description], index) => (
-                                                <div
-                                                    key={index}
-                                                    className="relative flex-auto mt-8 bg-no-repeat bg-cover bg-center min-h-[200px] max-h-[400px] w-full rounded-lg shadow-lg flex items-center justify-center group cursor-pointer"
-                                                    style={{ backgroundImage: `url(${img})` }}  data-aos="fade-up"
-                                                >
-                                                    {/* Hover Overlay */}
-                                                    <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center rounded-lg shadow-lg justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-50 transition-opacity duration-700">
-                                                        <p className="text-2xl">{title}</p>
-                                                        <p className="text-sm mt-2">{description}</p>
-                                                    </div>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="flex-auto text-center py-8" data-aos="fade-up">
-                                    <button className="px-6 py-2 md:py-4 md:px-8 rounded-full text-center text-black border-2 border-black hover:bg-black hover:text-white transition duration-700 cursor-pointer">
-                                        View More Portfolio
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="flex flex-col md:flex-row px-12 py-12" id="past" data-aos="fade-up">
-                                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-100 hover:opacity-90 ">
-                                    <FaBriefcase className="text-white size-16 my-4" />
-                                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">120</p>
-                                    <hr className="text-white w-32 my-4" />
-                                    <p className="text-center font-light font-sans text-white">Years on the Market</p>
-                                </div>
-                                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-80 hover:opacity-70 ">
-                                    <FaRegGrin className="text-white size-16 my-4"/>
-                                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">95</p>
-                                    <hr className="text-white w-32 my-4" />
-                                    <p className="text-center font-light font-sans text-white">Operational Brigades</p>
-                                </div>
-                                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-100 hover:opacity-90 ">
-                                    <FaChartBar className="text-white size-16 my-4"/>
-                                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">395</p>
-                                    <hr className="text-white w-32 my-4" />
-                                    <p className="text-center font-light font-sans text-white">Units Engineering</p>
-                                </div>
-                                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-80 hover:opacity-70 ">
-                                    <BsPersonFillGear className="text-white size-16 my-4"/>
-                                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">46</p>
-                                    <hr className="text-white w-32 my-4" />
-                                    <p className="text-center font-light font-sans text-white">Employees of the company</p>
-                                </div>
-                            </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex-auto text-center py-8" data-aos="fade-up">
+                    <button className="px-6 py-2 md:py-4 md:px-8 rounded-full text-center text-black border-2 border-black hover:bg-black hover:text-white transition duration-700 cursor-pointer">
+                        View More Portfolio
+                    </button>
+                </div>                
+            </div>
+            <div className="flex flex-col md:flex-row px-12 py-12" id="past" data-aos="fade-up">
+                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-100 hover:opacity-90 ">
+                    <FaBriefcase className="text-white size-16 my-4" />
+                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">120</p>
+                    <hr className="text-white w-32 my-4" />
+                    <p className="text-center font-light font-sans text-white">Years on the Market</p>
+                </div>
+                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-80 hover:opacity-70 ">
+                    <FaRegGrin className="text-white size-16 my-4"/>
+                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">95</p>
+                    <hr className="text-white w-32 my-4" />
+                    <p className="text-center font-light font-sans text-white">Operational Brigades</p>
+                </div>
+                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-100 hover:opacity-90 ">
+                    <FaChartBar className="text-white size-16 my-4"/>
+                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">395</p>
+                    <hr className="text-white w-32 my-4" />
+                    <p className="text-center font-light font-sans text-white">Units Engineering</p>
+                </div>
+                <div className="bg-black bg-cover py-16 w-full flex flex-col flex-1 items-center opacity-80 hover:opacity-70 ">
+                    <BsPersonFillGear className="text-white size-16 my-4"/>
+                    <p className="text-center font-semibold font-sans text-amber-500 text-[24px]">46</p>
+                    <hr className="text-white w-32 my-4" />
+                    <p className="text-center font-light font-sans text-white">Employees of the company</p>
+                </div>
+            </div>
                             <div className="flex flex-col px-12 py-12 bg-white bg-cover" id="past" data-aos="fade-up">
                                 <p className="text-black text-center text-[48px] font-serif">Our Creative Team</p>
                                 <p className="text-gray-500 m-8 text-center">A wonderful serenity has taken possession of my entire soul, like these sweet mo alone, and feel the charm of existence in this spot.</p>
@@ -281,10 +281,10 @@ const [currentIndex, setCurrentIndex] = useState(0);
                                     </button>
                                 </div>
                             </div>
-                            <div className="flex flex-col px-12 py-12 bg-white bg-cover" id="blogs">
-                                <p className="text-black text-[48px] text-center font-serif mt-4">Blogs</p>
-                                <div className=""></div>
-                            </div>
+            <div className="flex flex-col px-12 py-12 bg-white bg-cover" id="blogs">
+                    <p className="text-black text-[48px] text-center font-serif mt-4">Blogs</p>
+                <div className=""></div>
+            </div>
         </>
     );
 }
